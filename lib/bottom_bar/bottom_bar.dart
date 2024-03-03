c8d29862ea01511cd82f2d1d0fd3e1e5eda318ee
Bagrat Gharibjanyan <bagratgharibjanyan8@gmail.com>
2024-03-03 20:44:56 +0400

    test1

--- lib/bottom_bar/bottom_bar.dart
+++ lib/bottom_bar/bottom_bar.dart
@@ -1,4 +1,3 @@
-// custom_bottom_bar.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geometry_task/main.dart';
@@ -14,83 +13,88 @@ class CustomBottomBar extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return BottomAppBar(
-        color: Colors.transparent,
-        elevation: 0,
-        height: 160,
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 6.0,
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 10),
+      color: Colors.transparent,
+      elevation: 0,
+      height: 180,
+      shape: const CircularNotchedRectangle(),
+      notchMargin: 6.0,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  child: Container(
+                    padding: EdgeInsets.only(right: 20),
+                    margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
                     child: Padding(
                       padding: const EdgeInsets.only(left: 20),
-                      child: Text(message),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-              child: Container(
-                margin: const EdgeInsets.all(15),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 20)),
-                    elevation: MaterialStateProperty.all(0.0),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
+                      child: Text(
+                        message,
+                        style: const TextStyle(overflow: TextOverflow.ellipsis),
                       ),
                     ),
-                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Container(
+              margin: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 15, horizontal: 20)),
+                  elevation: MaterialStateProperty.all(0.0),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
                         (Set<MaterialState> states) {
                       if (states.contains(MaterialState.pressed)) {
                         return Colors.white;
                       }
                       return const Color(0xFFE3E3E3);
-                    }),
+                    },
                   ),
-                  onPressed: ref.read(figureDataProvider.notifier).clearOffsets,
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.cancel,
-                        color: Color(0xFF7D7D7D),
-                        size: 20,
-                      ),
-                      Text(
-                        'Отменить действие',
-                        style: TextStyle(
-                            color: Color(0xFF7D7D7D),
-                            height: 1.4,
-                            fontSize: 14),
-                      ),
-                    ],
-                  ),
+                ),
+                onPressed: ref.read(figureDataProvider.notifier).clearOffsets,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.cancel,
+                      color: Color(0xFF7D7D7D),
+                      size: 20,
+                    ),
+                    Text(
+                      'Отменить действие',
+                      style: TextStyle(
+                          color: Color(0xFF7D7D7D),
+                          height: 1.4,
+                          fontSize: 14),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geometry_task/figure_data_manager/figure_data_manager.dart';
 import 'package:geometry_task/main_screen/main_screen.dart';
 
-final figureDataProvider = StateNotifierProvider<FigureDataNotifier,FigureData>((ref) => FigureDataNotifier(FigureData(currentPosition: Offset(0,0))));
+final figureDataProvider = StateNotifierProvider<FigureDataNotifier,FigureData>((ref) => FigureDataNotifier(FigureData(currentPosition: const Offset(0,0))));
 
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child:  MyApp()));
 }
 
-class MyAgipp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
